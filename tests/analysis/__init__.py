a286885fcfcb9729892a9d80c11ca94ca355b30d
Fabian Frauenberger <ffrauenberger@swisdata.eu>
2026-01-13 22:23:21 +0100

    tests: fix naming of files

--- tests/analysis/__init__.py
+++ tests/analysis/__init__.py
@@ -1,7 +1,7 @@
 import unittest
 
-from .detection_model_tests import *
-from .prediction_model_tests import *
+from .detection_model_test import *
+from .prediction_model_test import *
 
 if __name__ == "__main__":
     unittest.main()
